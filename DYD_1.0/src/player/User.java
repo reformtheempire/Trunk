a7043ebbd5c4a4abe0d99af2236dc6338e62a78d
creativemario <thomasmather7@hotmail.co.uk>
2014-10-03 23:15:30 +0100

    added class player

---
+++ DYD_1.0/src/player/User.java
@@ -0,0 +1,63 @@
+package player;
+
+public class User {
+	private String name;
+	private String health;
+	private String race;
+	
+	private int strength;
+	private int agility;
+	
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getHealth() {
+		return health;
+	}
+
+	public void setHealth(String health) {
+		this.health = health;
+	}
+
+	public String getRace() {
+		return race;
+	}
+
+	public void setRace(String race) {
+		this.race = race;
+	}
+
+	public int getStrength() {
+		return strength;
+	}
+
+	public void setStrength(int strength) {
+		this.strength = strength;
+	}
+
+	public int getAgility() {
+		return agility;
+	}
+
+	public void setAgility(int agility) {
+		this.agility = agility;
+	}
+
+	public User(String name, String health, String race, int strength,
+			int agility) {
+		super();
+		this.name = name;
+		this.health = health;
+		this.race = race;
+		this.strength = strength;
+		this.agility = agility;
+	}
+	
+	
+	
+}
