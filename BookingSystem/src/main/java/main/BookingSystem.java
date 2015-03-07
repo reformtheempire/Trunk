bb87c83c0489330cdae5727ac6c7803b8d4960bd
creativemario <thomasmather7@hotmail.co.uk>
2015-03-07 12:37:12 +0000

    Booking system modification to main

--- BookingSystem/src/main/java/main/BookingSystem.java
+++ BookingSystem/src/main/java/main/BookingSystem.java
@@ -3,6 +3,9 @@ package main;
 import java.awt.EventQueue;
 import java.sql.Connection;
 
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+
 import users.User;
 import window.ControlPanel;
 import window.LoginScreen;
@@ -16,6 +19,22 @@ public class BookingSystem {
 	public static User user;
 	static ControlPanel window;
 	public static void main(String[] args) {
+		try {
+			UIManager.setLookAndFeel("com.sun.java.swing.plaf.windows.WindowsLookAndFeel");
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InstantiationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (UnsupportedLookAndFeelException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
 		LoginScreen loginScreen = new LoginScreen();
 		loginScreen.show();
 		
